b0b543c3fa99e42f3f395dbd0509778fd78017e8
August Toman-Yih <august.toman.yih@gmail.com>
2015-09-28 20:47:14 -0700

    adds twilights to altitude meter

--- js/zenometer.js
+++ js/zenometer.js
@@ -20,7 +20,11 @@ const Zenometer = React.createClass({
         <rect className="area above-horizon" x={0} y={0} width={width} height={height/2} />
         <rect className="area below-horizon" x={0} y={height/2} width={width} height={height/2} />
 
-        <path className="pin" d={`M ${0} ${pinPosition} L ${width} ${pinPosition}`}> </path>
+        <rect className="area civil-twilight" x={0} y={height/2} width={width} height={6/180 * height} />
+        <rect className="area nautical-twilight" x={0} y={height/2 + 6/180 * height} width={width} height={6/180 * height} />
+        <rect className="area astronomical-twilight" x={0} y={height/2 + 12/180 * height} width={width} height={6/180 * height} />
+
+        <path className="pin" d={`M ${0} ${pinPosition} L ${width} ${pinPosition}`} />
       </svg>
     );
   }
