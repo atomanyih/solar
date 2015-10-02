0ffec197cf47117c29465aa8385484139d187a65
August Toman-Yih <atomanyih@pivotal.io>
2015-10-02 11:51:33 -0700

    make it easier to change latitude and longitude

--- js/astronomy.js
+++ js/astronomy.js
@@ -1,21 +1,23 @@
 const SunCalc = require('suncalc');
 
+const coordinates = [37, -122];
+
 export function getSunTimes(time) {
-  return SunCalc.getTimes(time.date, 37, -122);
+  return SunCalc.getTimes(time.date, ...coordinates);
 }
 
 export function getMoonTimes(time) {
-  return SunCalc.getMoonTimes(time.date, 37, -122);
+  return SunCalc.getMoonTimes(time.date, ...coordinates);
 }
 
 export function getSunPosition(time) {
-  return SunCalc.getPosition(time.date, 37, -122);
+  return SunCalc.getPosition(time.date, ...coordinates);
 }
 
 export function getSunPositionRange(time) {
-  const times = SunCalc.getTimes(time.date, 37, -122);
-  const maxPosition = SunCalc.getPosition(times.solarNoon, 37, -122).altitude;
-  const minPosition = SunCalc.getPosition(times.nadir, 37, -122).altitude;
+  const times = SunCalc.getTimes(time.date, ...coordinates);
+  const maxPosition = SunCalc.getPosition(times.solarNoon, ...coordinates).altitude;
+  const minPosition = SunCalc.getPosition(times.nadir, ...coordinates).altitude;
 
   return [minPosition, maxPosition];
 }
